9099915e56159268220b8ed3c48518e225d118d9
Bún Bò đáng iu <145783665+Ngleanthu@users.noreply.github.com>
2025-01-14 17:58:13 +0700

    Admin func

--- app.js
+++ app.js
@@ -21,12 +21,6 @@ const SESSION_SECRET = process.env.SESSION_SECRET;
 
 
 const app = express();
-
-// const store = new MongoDBStore( {
-//     uri: MONGO_URI,
-//     collection: 'sessions'
-// })
-// Set view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -42,42 +36,29 @@ app.use(
         saveUninitialized: false,
     })
 );
-app.use(flash());
 
+app.use(flash());
 // Khởi tạo Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
-    // User.findById("678554e7b5dc05401b8a81ed").then(user =>{
-    //     res.locals.user = user || null; 
-    //     next();
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    //     res.locals.user = null;  // Gán null nếu có lỗi
-    //     next();
-    // });
     if (req.isAuthenticated()) {
-        res.locals.user = req.user;  // Truyền thông tin user cho tất cả các view
+        res.locals.user = req.user;  
     } else {
-        res.locals.user = null;  // Nếu chưa đăng nhập, user là null
+        res.locals.user = null;  
     }
     next();
 });
-
-
+app.use((req, res, next) => {
+    res.locals.errorMessages = req.flash('error');  
+    next();
+});
 
 app.use(productRoutes);
 app.use('/admin', adminRoutes);
 app.use(authRoutes);
 
-// Admin dashboard and pages
-// app.get('/admin/dashboard', (req, res) => res.render('admin/dashboard', { path: '/dashboard' }));
-// app.get('/manage-users', (req, res) => res.render('admin/manage-users', { path: '/manage-users' }));
-
-// User pages
 app.get('/', (req, res) => res.render('user/index', { path: '/'}));
-
 app.get('/about', (req, res) => res.render('user/about', { path: '/about' }));
 app.get('/blog', (req, res) => res.render('user/blog', { path: '/blog' }));
 app.get('/blog-details', (req, res) => res.render('user/blog-details', { path: '/blog-details' }));
