9112826e491194a95106cd41f8cddc08535d372a
Bún Bò đáng iu <145783665+Ngleanthu@users.noreply.github.com>
2025-01-14 20:09:00 +0700

    final check

---
+++ config/mailer.js
@@ -0,0 +1,12 @@
+// const nodemailer = require('nodemailer');
+
+// // Tạo transporter với thông tin tài khoản Gmail
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'your-email@gmail.com', // Địa chỉ email của bạn
+//     pass: 'your-email-password',  // Mật khẩu của Gmail hoặc mật khẩu ứng dụng (xem thêm dưới đây)
+//   },
+// });
+
+// module.exports = transporter;
